ef89012aabc19b18472d00a59539427c6e66050e
Rafael Carballo <rcarballo@emergya.com>
2018-01-18 21:54:42 +0100

    V2

--- src/src/main/java/s4c/microservices/authorization/Application.java
+++ src/src/main/java/s4c/microservices/authorization/Application.java
@@ -2,12 +2,21 @@ package s4c.microservices.authorization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 
 @EnableZuulProxy
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }   
